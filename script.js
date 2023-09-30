a5707217ae62a35ba730f6d82fb83aded58bd41c
SUNDARRAJAN S <142830392+sundar-mern@users.noreply.github.com>
2023-09-30 17:06:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+setTimeout(() => {
+    document.getElementById('callback').innerHTML = 10;
+    setTimeout(() => {
+        document.getElementById('callback').innerHTML = 9;
+        setTimeout(() => {
+            document.getElementById('callback').innerHTML=8
+            setTimeout(() => {
+                document.getElementById('callback').innerHTML=7
+                setTimeout(() => {
+                    document.getElementById('callback').innerHTML=6
+                    setTimeout(() => {
+                        document.getElementById('callback').innerHTML=5
+                        setTimeout(() => {
+                            document.getElementById('callback').innerHTML=4
+                            setTimeout(() => {
+                                document.getElementById('callback').innerHTML=3
+                                setTimeout(() => {
+                                    document.getElementById('callback').innerHTML=2
+                                    setTimeout(() => {
+                                        document.getElementById('callback').innerHTML=1
+                                        setTimeout(() => {
+                                            document.getElementById('callback').innerHTML= "HAPPY INDEPENDENCE DAY"
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000)
+}, 1000)
+    
+   
